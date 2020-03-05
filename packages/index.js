2ca3383fee90d995edf822d23634e1180430c65b
xiaolu001 <136667423@qq.com>
2020-03-05 18:11:00 +0800

    updata

--- packages/index.js
+++ packages/index.js
@@ -1,3 +1,5 @@
-import depth from "./chart/RootChart"
-
-export default depth
+import depthChart from "./chart/RootChart"
+depthChart.install = function (Vue) {
+    Vue.component("depthChart", depthChart)
+}  
+export default depthChart
