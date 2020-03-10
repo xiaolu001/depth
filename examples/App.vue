8eb7a092930d688ba77397fec33c13bd841387a7
xiaolu001 <136667423@qq.com>
2020-03-10 18:24:10 +0800

    updata

--- examples/App.vue
+++ examples/App.vue
@@ -11,7 +11,9 @@ export default {
   name: "App",
   data() {
     return {
-      depth: null
+      depth: null,
+      throttle:'',
+      onResize:''
     };
   },
   mounted() {
@@ -22,7 +24,26 @@ export default {
       data: data,
       locale: "zh"
     });
-  }
+    this.throttle = (func, wait) => {
+      let previous = 0;
+      return () => {
+        const now = Date.now();
+        if (now - previous > wait) {
+          func();
+          previous = now;
+        }
+      };
+    };
+    this.onResize = this.throttle(() => {
+      if (this.depth) {
+        this.depth.updata(data);
+      }
+    }, 1000 / 16);
+    window.addEventListener("resize", this.onResize, false);
+  },
+  // methods: {
+  //   onResize() {}
+  // }
 };
 </script>
 
